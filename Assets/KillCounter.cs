4ff6eb4785c513f89120114ff2b62d7b51d4906a
David Neilsen <david@panmedia.co.nz>
2014-01-25 09:40:57 +1300

    Add UI

---
+++ Assets/KillCounter.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillCounter : MonoBehaviour {
+
+    private int killCount;
+    public int KillCount {
+        get {
+            return killCount;
+        }
+    }
+
+	public void Start () {
+        killCount = Mathf.FloorToInt(Random.Range(0, 10));
+	}
+	
+}
---
+++ Assets/RespawnUI.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RespawnUI : MonoBehaviour {
+
+    public Texture player1Head;
+
+    public GameObject player1;
+    public GameObject player2;
+    public GameObject player3;
+    public GameObject player4;
+    
+    public LavaDeath player1Respawn;
+    public LavaDeath player2Respawn;
+    public LavaDeath player3Respawn;
+    public LavaDeath player4Respawn;
+    
+    public KillCounter player1KillCounter;
+    public KillCounter player2KillCounter;
+    public KillCounter player3KillCounter;
+    public KillCounter player4KillCounter;
+
+	public void Start () {
+	    foreach (GameObject gameObject in GameObject.FindGameObjectsWithTag("Player")) {
+            if (gameObject.name == "Player1") {
+                player1 = gameObject;
+                player1Respawn = player1.GetComponent<LavaDeath>();
+                player1KillCounter = player1.GetComponent<KillCounter>();
+            } else if (gameObject.name == "Player2") {
+                player2 = gameObject;
+                player2Respawn = player2.GetComponent<LavaDeath>();
+                player2KillCounter = player2.GetComponent<KillCounter>();
+            } else if (gameObject.name == "Player3") {
+                player3 = gameObject;
+                player3Respawn = player3.GetComponent<LavaDeath>();
+                player3KillCounter = player3.GetComponent<KillCounter>();
+            } else if (gameObject.name == "Player4") {
+                player4 = gameObject;
+                player4Respawn = player4.GetComponent<LavaDeath>();
+                player4KillCounter = player4.GetComponent<KillCounter>();
+            }
+        }
+  	}
+
+    public string GetGUIText(LavaDeath respawn, KillCounter killCounter) {
+        string result = "";
+        if (respawn.Respawning > 0) {
+            result += "Respawning in: " + respawn.Respawning;
+        }
+        result += "Kill counter: " + killCounter.KillCount;
+        return result;
+    }
+
+    public void OnGUI(){
+        int width = 150;
+        GUI.Box(new Rect(0, 0, width, 50), GetGUIText(player1Respawn, player1KillCounter));
+        GUI.Box(new Rect(Screen.width - width, 0, width, 50), GetGUIText(player2Respawn, player2KillCounter));
+        GUI.Box(new Rect(0,Screen.height - 50, width, 50), GetGUIText(player3Respawn, player3KillCounter));
+        GUI.Box(new Rect(Screen.width - width, Screen.height - 50, width, 50), GetGUIText(player4Respawn, player4KillCounter));
+
+        for (int i = 0; i < player1KillCounter.KillCount; i++) {
+            GUI.DrawTexture(new Rect(15 * i, 15, 15, 31), player1Head);
+        }
+    }
+}
+
