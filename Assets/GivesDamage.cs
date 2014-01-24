8a103a37bd5157e5cfc5e7f41e6b602c18684f1a
Michael <linuxfornerds@gmail.com>
2014-01-25 01:22:54 +1300

    Commit

---
+++ Assets/GivesDamage.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GivesDamage : MonoBehaviour {
+
+	public void OnCollisionEnter(Collision collision) {
+		collision.gameObject.SendMessage ("Damage", 1, SendMessageOptions.DontRequireReceiver);
+	}
+}
