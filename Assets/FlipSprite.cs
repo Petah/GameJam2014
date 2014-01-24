fd129136286a291f0388b07c801ecce262363765
David Neilsen <david@panmedia.co.nz>
2014-01-25 00:20:47 +1300

    Add flip sprite

---
+++ Assets/FlipSprite.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlipSprite : MonoBehaviour {
+
+    private float lastX = 0;
+    private float scale;
+
+    public void Start() {
+        scale = transform.localScale.x;
+    }
+	
+	public void Update () {
+        Vector3 localScale = transform.localScale;
+        if (transform.position.x < lastX) {
+            localScale.x = -scale;
+        } else if (transform.position.x > lastX) {
+            localScale.x = scale;
+        }
+        transform.localScale = localScale;
+        lastX = transform.position.x;
+	}
+
+}
