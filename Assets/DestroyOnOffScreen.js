afe01c2c537685a7ebffb4abec302e6092f02288
Matt Grey <i_live_in_a_sock_drawer@hotmail.com>
2014-01-25 09:15:10 +1300

    Update

---
+++ Assets/DestroyOnOffScreen.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+var cameraFocus:Transform;
+var dropOffset:float = 1.0;
+function Update () {
+if(cameraFocus.position.y > transform.position.y + dropOffset){
+Destroy(gameObject);
+}
+}
+
+function OnBecameInvisible () {
+
+}
