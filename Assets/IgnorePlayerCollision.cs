18b8a3b76ad3bc041569e707f890b433ca34e906
David Neilsen <david@panmedia.co.nz>
2014-01-24 22:14:45 +1300

    Update

---
+++ Assets/IgnorePlayerCollision.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IgnorePlayerCollision : MonoBehaviour {
+
+	public void Start () {
+        foreach (GameObject player in GameObject.FindGameObjectsWithTag("Player")) {
+            Physics.IgnoreCollision(collider, player.collider);
+        }
+	}
+	
+}
---
+++ Assets/Pickup.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pickup : MonoBehaviour {
+
+    public GameObject[] players;
+
+    public void Start() {
+        players = GameObject.FindGameObjectsWithTag("Player");
+    }
+
+    public void FixedUpdate() {
+        foreach (GameObject player in players) {
+            float distance = Vector3.Distance(transform.position, player.transform.position);
+            if (distance < 1) {
+//                Destroy(gameObject);
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.tag.Equals("Player")) {
+            Debug.Log(collision);
+            Destroy(gameObject);
+        }
+    }
+
+}
