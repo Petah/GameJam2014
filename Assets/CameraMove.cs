7d02fa840ee2d2fd6ec10ab0fc12f3e3c212fec0
David Neilsen <david@panmedia.co.nz>
2014-01-25 00:20:39 +1300

    Add cam move

---
+++ Assets/CameraMove.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMove : MonoBehaviour {
+    
+    public float speed = 1.0F;
+    private Vector3 startMarker;
+    private Vector3 endMarker;
+    private float startTime;
+    private float journeyLength;
+
+    public void Start() {
+        startMarker = transform.position;
+        endMarker = new Vector3(transform.position.x, transform.position.y + 100, transform.position.z);
+        startTime = Time.time;
+        journeyLength = Vector3.Distance(transform.position, endMarker);
+    }
+
+    public void Update() {
+        float distCovered = (Time.time - startTime) * speed;
+        float fracJourney = distCovered / journeyLength;
+        transform.position = Vector3.Lerp(startMarker, endMarker, fracJourney);
+    }
+
+}
