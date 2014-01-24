2ca68f32bccef0c5c23dcaa6ed61b1b43724447b
David Neilsen <david@panmedia.co.nz>
2014-01-25 00:21:27 +1300

    Add melee attack

---
+++ Assets/MeleeAttack.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeleeAttack : MonoBehaviour {
+
+    public void Start() {
+    
+    }
+    
+    public void Update() {
+    
+    }
+
+    public void OnGizmoDraw() {
+        Gizmos.DrawSphere(transform.position, 1);
+    }
+
+}
