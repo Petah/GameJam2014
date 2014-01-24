129954ae904ec5deb99d12ab3dbe5d3261edf0b0
David Neilsen <david@panmedia.co.nz>
2014-01-25 03:53:38 +1300

    Add more

---
+++ Assets/Scripts/Impact.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Impact {
+    
+    public Vector3 direction;
+    public float force;
+
+    public Impact(Vector3 direction, float force) {
+        this.direction = direction;
+        this.force = force;
+    }
+
+}
