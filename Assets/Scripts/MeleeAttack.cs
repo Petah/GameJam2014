c0b307115ebdf79ce9bf31fdc0c5676886619a30
David Neilsen <david@panmedia.co.nz>
2014-01-25 00:44:04 +1300

    Update

---
+++ Assets/Scripts/MeleeAttack.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeleeAttack : MonoBehaviour {
+
+    public void Start() {
+    
+    }
+    
+    public void Update() {
+    
+    }
+
+    public void OnDrawGizmosSelected() {
+        Gizmos.color = Color.yellow;
+        Vector3 position = transform.position;
+        for (float i = 0; i < 0.6; i += 0.1f) {
+            position.x = Mathf.Cos(i);
+            position.y = Mathf.Sin(i);
+            Gizmos.DrawSphere(position, 0.1f);
+        }
+    }
+
+}
