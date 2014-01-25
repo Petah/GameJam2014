8ac85a4a1d956ff1cd19935034c4cc3221b08fa5
David Neilsen <david@panmedia.co.nz>
2014-01-25 15:34:57 +1300

    Add Follow.cs

---
+++ Assets/Follow.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follow : MonoBehaviour {
+
+    public Transform follow;
+
+    public void Update() {
+        transform.position = follow.position;
+    }
+
+}
